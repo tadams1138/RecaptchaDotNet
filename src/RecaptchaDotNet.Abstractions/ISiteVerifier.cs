acc96743058583556d21fabef042bfac461c4735
Thomas Adams <tadams1138@hotmail.com>
2021-02-19 12:01:01 -0500

    add abstractions

---
+++ src/RecaptchaDotNet.Abstractions/ISiteVerifier.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace RecaptchaDotNet.Abstractions
+{
+    public interface ISiteVerifier
+    {
+        Task<VerificationResponse> VerifyAsync(string response);
+    }
+}
---
+++ src/RecaptchaDotNet.Abstractions/VerificationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace RecaptchaDotNet.Abstractions
+{
+    public class VerificationResponse
+    {
+        public bool Success { get; set; }
+        public string HostName { get; set; }
+    }
+}
