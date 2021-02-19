cfb6b40a577298587271f312c775590441d2e9c2
Thomas Adams <tadams1138@hotmail.com>
2021-02-19 12:15:49 -0500

    add ValidateRecaptchaAttribute

---
+++ src/RecaptchaDotNet.AspNetCore.Mvc/ValidateRecaptchaAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RecaptchaDotNet.Abstractions;
+
+namespace RecaptchaDotNet.AspNetCore.Mvc
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public class ValidateRecaptchaAttribute : Attribute, IFilterFactory
+    {
+        public bool IsReusable => true;
+
+        public IFilterMetadata CreateInstance(IServiceProvider services)
+        {
+            var verifySite = services.GetService(typeof(ISiteVerifier)) as ISiteVerifier;
+            var options = services.GetService(typeof(ValidateRecaptchaFilterOptions)) as ValidateRecaptchaFilterOptions;
+            return new ValidateRecaptchaFilter(verifySite, options);
+        }
+    }
+}
